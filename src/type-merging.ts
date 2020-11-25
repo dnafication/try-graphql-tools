87495e17f43f6ba1edecb580f8a3b3357d62e65b
dnafication <dnafication@users.noreply.github.com>
2020-11-25 17:48:31 +1100

    Create type-merging.ts

---
+++ src/type-merging.ts
@@ -0,0 +1,47 @@
+import {makeExecutableSchema} from '@graphql-tools/schema';
+import {addMocksToSchema} from '@graphql-tools/mock';
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {mergeSchemas} from 'graphql-tools';
+import {makeApolloServer} from './util/makeServer';
+
+let postsSchema = makeExecutableSchema({
+  typeDefs: `
+    type Post {
+      id: ID!
+      message: String!
+      author: User!
+    }
+
+    type User {
+      id: ID!
+      posts: [Post]!
+    }
+
+    type Query {
+      postById(id: ID!): Post
+      postUserById(id: ID!): User
+    }
+  `,
+});
+
+let usersSchema = makeExecutableSchema({
+  typeDefs: `
+    type User {
+      id: ID!
+      email: String!
+    }
+
+    type Query {
+      userById(id: ID!): User
+    }
+  `,
+});
+
+// just mock the schemas for now to make them return dummy data
+postsSchema = addMocksToSchema({schema: postsSchema});
+usersSchema = addMocksToSchema({schema: usersSchema});
+
+const mergedSchema = mergeSchemas({schemas: [postsSchema, usersSchema]});
+
+makeApolloServer(mergedSchema, 5001);
