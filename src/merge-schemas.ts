f0049a0276cd42ffc113172d7fdb223932b9430c
dnafication <dnafication@users.noreply.github.com>
2020-11-25 17:48:23 +1100

    Create merge-schemas.ts

---
+++ src/merge-schemas.ts
@@ -0,0 +1,21 @@
+import authorTypes from './schema';
+import postTypes from './posts-service/schema';
+import {mergeTypeDefs} from 'graphql-tools';
+import {print} from 'graphql';
+
+const extendedType = `
+    type Author {
+      posts: [Post]
+    }
+
+    type Post {
+      author: Author
+    }
+`;
+
+const types = [authorTypes, postTypes, extendedType];
+
+const mergedTypes = mergeTypeDefs(types);
+
+console.log(mergedTypes);
+console.log(print(mergedTypes));
